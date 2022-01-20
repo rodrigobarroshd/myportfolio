2a694c51f8b4cb1913eaaf4f00f9b45134e5a52f
Rodrigo Barros <50782890+rodrigobarroshd@users.noreply.github.com>
2022-01-20 18:39:17 -0300

    Add files via upload

---
+++ Hangman Game/desafio_jogo_da_forca.py
@@ -0,0 +1,43 @@
+secreto = input( 'Qual palavra estará na forca? ')
+digitadas = []
+chances = 2
+
+while True:
+    if chances <= 0:
+        print('Vish... Você Perdeu !') 
+        break
+    
+    letra = input('Digite uma letra: ')
+    
+    if len(letra) > 1:
+        print('Isso não vale, digite apenas uma letra. ')
+        continue
+    
+    digitadas.append(letra)
+    print(digitadas)
+    
+    if letra in secreto:
+        print(f'WoooW, a letra {letra} existe na palavra secreta')
+    else:
+        print(f'Aff, a letra {letra} não existe na palavra secreta')
+        digitadas.pop()
+        
+    secreto_temporario = ''
+    for letra_secreta in secreto:
+        if letra_secreta in digitadas:
+            secreto_temporario += letra_secreta
+        else:
+            secreto_temporario += '*'
+            
+    if secreto_temporario == secreto:
+        print(f' WOOOOOWW, Você acaba de ganhar!!!! A palavra era "{secreto_temporario}" !!')
+        break
+    else:
+        print(f'A palavra secreta está assim: {secreto_temporario}')
+        print()
+    if letra not in secreto:
+        chances -= 1  
+    print(f'Cuidado, Você ainda tem {chances} chances...')
+    
+
+    print()
