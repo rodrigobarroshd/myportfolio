0d38bcb4c37ceb9514a7957337c2d3aac6870a53
Rodrigo Barros <50782890+rodrigobarroshd@users.noreply.github.com>
2022-01-24 14:31:21 -0300

    Add files via upload

---
+++ cpf_geracao_validacao/geracao_cpf.py
@@ -0,0 +1,27 @@
+from random import randint
+numero = str(randint(100000000, 999999999))
+
+
+
+novo_cpf = numero             
+reverso = 10                        # Contador reverso
+total = 0
+
+# Loop do CPF
+for index in range(19):
+    if index > 8:                   
+        index -= 9                  
+
+    total += int(novo_cpf[index]) * reverso  # Valor total da multiplicação
+
+    reverso -= 1                    
+    if reverso < 2:
+        reverso = 11
+        d = 11 - (total % 11)
+
+        if d > 9:                   
+            d = 0
+        total = 0                   
+        novo_cpf += str(d)          
+
+print(novo_cpf)
